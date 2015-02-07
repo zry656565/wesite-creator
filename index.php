76a016fa03e6847f73537ec561cc29c53804c990
Jerry Zou <zry656565@gmail.com>
2015-02-07 21:48:14 +0800

    initialize the "hello world" page

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * User: Jerry
+ * Date: 15/2/7
+ */
+$title = 'Hello World';
+?>
+
+<!doctype html>
+<html lang="zh">
+<head>
+	<meta charset="UTF-8">
+	<title><?= $title ?></title>
+	<link rel="stylesheet" href="assets/library/idangerous.swiper/idangerous.swiper.css">
+	<link rel="stylesheet" href="assets/stylesheet/wesite.css">
+</head>
+<body>
+
+	<!-- script -->
+	<script src="assets/library/idangerous.swiper/idangerous.swiper.js"></script>
+</body>
+</html>
