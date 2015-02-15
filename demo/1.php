34d487e2965f3b81ed21c4490b789e1e5e320326
Jerry Zou <zry656565@gmail.com>
2015-02-15 22:53:42 +0800

    为了低版本的php做些改变

--- demo/1.php
+++ demo/1.php
@@ -3,24 +3,24 @@
 $demo = true;
 
 $pageName = 'DEMO PAGE';
-$data = [
-	'slides' => [
-		[
+$data = array(
+	'slides' => array(
+		array(
 			'background' => 'http://women-image.b0.upaiyun.com/demo/1/1.jpg',
-		], [
+		), array(
 			'background' => 'http://women-image.b0.upaiyun.com/demo/1/3.jpg',
-		], [
+		), array(
 			'background' => 'http://women-image.b0.upaiyun.com/demo/1/5.jpg',
-		], [
+		), array(
 			'background' => 'http://women-image.b0.upaiyun.com/demo/1/25k.jpg',
-		], [
+		), array(
 			'background' => 'http://women-image.b0.upaiyun.com/demo/1/30min.jpg',
-		], [
+		), array(
 			'background' => 'http://women-image.b0.upaiyun.com/demo/1/30y.jpg',
-		], [
+		), array(
 			'background' => 'http://women-image.b0.upaiyun.com/demo/1/80.jpg',
-		],
-	]
-];
+		),
+	)
+);
 
 include('../show/demo.php');
