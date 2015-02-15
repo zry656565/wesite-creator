415074d54ab04d0ddd6f11eeb41291710299661b
Jerry Zou <zry656565@gmail.com>
2015-02-15 22:54:03 +0800

    修复一个显示上的问题

--- show/demo.php
+++ show/demo.php
@@ -3,9 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title><?= $pageName ?></title>
-	<meta name="viewport" content="width=device-width, target-densityDpi=medium-dpi, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
-	<meta name="apple-mobile-web-app-capable" content="yes">
-	<meta name="apple-mobile-web-app-status-bar-style" content="black">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
 	<meta name="format-detection" content="telephone=no">
 	<link rel="stylesheet" href="../assets/library/idangerous.swiper/idangerous.swiper.css">
 	<link rel="stylesheet" href="../assets/stylesheet/wesite.css">
