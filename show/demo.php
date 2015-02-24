3052aaee32da54a3b8a63b817acce798dddd6c6a
Jerry Zou <zry656565@gmail.com>
2015-02-25 02:48:35 +0800

    demo 1

--- show/demo.php
+++ show/demo.php
@@ -6,13 +6,21 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
 	<meta name="format-detection" content="telephone=no">
 	<link rel="stylesheet" href="../assets/library/idangerous.swiper/idangerous.swiper.css">
-	<link rel="stylesheet" href="../assets/stylesheet/show.css?v=1.0.4">
-	<?= isset($extentStyle) ? $extentStyle : '' ?>
+	<link rel="stylesheet" href="../assets/stylesheet/show.css">
+	<?php
+	if (isset($id) && $id === 1) { ?>
+	<link rel="stylesheet" href="../assets/stylesheet/demo/1.css">
+	<?php
+	} ?>
 	<!-- script -->
 	<script src="../assets/library/jquery/jquery-1.11.2.min.js"></script>
 	<script src="../assets/library/idangerous.swiper/idangerous.swiper.js"></script>
 	<script src="../assets/library/spin/spin.js"></script>
-	<script src="../assets/javascript/wesite.js?v=1.0.4"></script>
+	<?php
+	if (isset($id) && $id === 1) { ?>
+		<script src="../assets/javascript/demo/1.js"></script>
+	<?php
+	} ?>
 </head>
 <body>
 
@@ -26,13 +34,15 @@
 	<div class="swiper-wrapper">
 		<?php
 		foreach ($data['slides'] as $slide) { ?>
-			<div class="swiper-slide">
+			<div class="swiper-slide" <?= isset($slide['id']) ? "data-id=\"{$slide['id']}\"" : "" ?>>
 				<img class="music-icon" src="../assets/images/music.png"/>
 				<img class="background" src="<?= $slide['background'] ?>" />
-				<div class="top sub"></div>
-				<div class="header sub"></div>
-				<div class="body sub"></div>
-				<div class="footer sub"></div>
+				<?php if (isset($slide['id'])) { ?>
+				<img class="header sub" src="<?= $slide['header'] ?>"/>
+				<img class="body sub" src="<?= $slide['body'] ?>"/>
+				<img class="footer sub" src="<?= $slide['footer'] ?>"/>
+				<?php
+				} ?>
 				<img class="arrow" src="../assets/images/arrow.gif" />
 			</div>
 		<?php
