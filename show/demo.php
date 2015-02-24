2e66dd746a0557c9fad5797cefbb4bdbf450a2de
Jerry Zou <zry656565@gmail.com>
2015-02-25 01:17:02 +0800

    添加四部分的定位

--- show/demo.php
+++ show/demo.php
@@ -6,7 +6,8 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
 	<meta name="format-detection" content="telephone=no">
 	<link rel="stylesheet" href="../assets/library/idangerous.swiper/idangerous.swiper.css">
-	<link rel="stylesheet" href="../assets/stylesheet/show.css?v=1.0.3">
+	<link rel="stylesheet" href="../assets/stylesheet/show.css?v=1.0.4">
+	<?= isset($extentStyle) ? $extentStyle : '' ?>
 	<!-- script -->
 	<script src="../assets/library/jquery/jquery-1.11.2.min.js"></script>
 	<script src="../assets/library/idangerous.swiper/idangerous.swiper.js"></script>
@@ -28,6 +29,10 @@
 			<div class="swiper-slide">
 				<img class="music-icon" src="../assets/images/music.png"/>
 				<img class="background" src="<?= $slide['background'] ?>" />
+				<div class="top sub"></div>
+				<div class="header sub"></div>
+				<div class="body sub"></div>
+				<div class="footer sub"></div>
 				<img class="arrow" src="../assets/images/arrow.gif" />
 			</div>
 		<?php
