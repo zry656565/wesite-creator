faaa8f0527592c84be83252cbeb0234bd305bd41
Jerry Zou <zry656565@gmail.com>
2015-02-28 21:45:30 +0800

    调整 demo 1 页面的顺序

--- demo/1.php
+++ demo/1.php
@@ -9,31 +9,6 @@ $music = '//women-music.b0.upaiyun.com/demo/sunrise_little.mp3';
 $data = array(
 	'slides' => array(
 		array(
-			'background' => $assetRoot . '1_bg.png',
-			'asset' => array(
-				array(
-					'src' => $assetRoot . '1_1.png',
-					'width' => '22.34',
-					'height' => '1.33',
-					'top' => '4',
-					'left' => '38.84',
-				),
-				array(
-					'src' => $assetRoot . '1_2.png',
-					'width' => '44.23',
-					'height' => '49.91',
-					'top' => '21',
-					'left' => '30',
-				),
-				array(
-					'src' => $assetRoot . '1_3.png',
-					'width' => '75.44',
-					'height' => '15.48',
-					'top' => '78',
-					'left' => '12.28',
-				),
-			),
-		), array(
 			'background' => $assetRoot . '3_bg.png',
 			'asset' => array(
 				array(
@@ -56,56 +31,6 @@ $data = array(
 					'height' => '12.01',
 					'top' => '81',
 					'left' => '21.3',
-				),
-			),
-		), array(
-			'background' => $assetRoot . '5_bg.png',
-			'asset' => array(
-				array(
-					'src' => $assetRoot . '5_1.png',
-					'width' => '43.34',
-					'height' => '1.33',
-					'top' => '4',
-					'left' => '28.33',
-				),
-				array(
-					'src' => $assetRoot . '5_2.png',
-					'width' => '61.39',
-					'height' => '55.96',
-					'top' => '17',
-					'left' => '23.3',
-				),
-				array(
-					'src' => $assetRoot . '5_3.png',
-					'width' => '75.44',
-					'height' => '15.84',
-					'top' => '78',
-					'left' => '12.28',
-				),
-			),
-		), array(
-			'background' => $assetRoot . '25_bg.png',
-			'asset' => array(
-				array(
-					'src' => $assetRoot . '25_1.png',
-					'width' => '33.73',
-					'height' => '1.69',
-					'top' => '4',
-					'left' => '33.14',
-				),
-				array(
-					'src' => $assetRoot . '25_2.png',
-					'width' => '90.38',
-					'height' => '39.23',
-					'top' => '30',
-					'left' => '6.7',
-				),
-				array(
-					'src' => $assetRoot . '25_3.png',
-					'width' => '75.44',
-					'height' => '15.84',
-					'top' => '78',
-					'left' => '12.28',
 				),
 			),
 		), array(
@@ -133,6 +58,106 @@ $data = array(
 					'left' => '20.78',
 				),
 			),
+		),  array(
+			'background' => $assetRoot . '25_bg.png',
+			'asset' => array(
+				array(
+					'src' => $assetRoot . '25_1.png',
+					'width' => '33.73',
+					'height' => '1.69',
+					'top' => '4',
+					'left' => '33.14',
+				),
+				array(
+					'src' => $assetRoot . '25_2.png',
+					'width' => '90.38',
+					'height' => '39.23',
+					'top' => '30',
+					'left' => '6.7',
+				),
+				array(
+					'src' => $assetRoot . '25_3.png',
+					'width' => '75.44',
+					'height' => '15.84',
+					'top' => '78',
+					'left' => '12.28',
+				),
+			),
+		), array(
+			'background' => $assetRoot . '1_bg.png',
+			'asset' => array(
+				array(
+					'src' => $assetRoot . '1_1.png',
+					'width' => '22.34',
+					'height' => '1.33',
+					'top' => '4',
+					'left' => '38.84',
+				),
+				array(
+					'src' => $assetRoot . '1_2.png',
+					'width' => '44.23',
+					'height' => '49.91',
+					'top' => '21',
+					'left' => '30',
+				),
+				array(
+					'src' => $assetRoot . '1_3.png',
+					'width' => '75.44',
+					'height' => '15.48',
+					'top' => '78',
+					'left' => '12.28',
+				),
+			),
+		), array(
+			'background' => $assetRoot . '5_bg.png',
+			'asset' => array(
+				array(
+					'src' => $assetRoot . '5_1.png',
+					'width' => '43.34',
+					'height' => '1.33',
+					'top' => '4',
+					'left' => '28.33',
+				),
+				array(
+					'src' => $assetRoot . '5_2.png',
+					'width' => '61.39',
+					'height' => '55.96',
+					'top' => '17',
+					'left' => '23.3',
+				),
+				array(
+					'src' => $assetRoot . '5_3.png',
+					'width' => '75.44',
+					'height' => '15.84',
+					'top' => '78',
+					'left' => '12.28',
+				),
+			),
+		),  array(
+			'background' => $assetRoot . '80_bg.png',
+			'asset' => array(
+				array(
+					'src' => $assetRoot . '80_1.png',
+					'width' => '41.12',
+					'height' => '1.33',
+					'top' => '4',
+					'left' => '29.44',
+				),
+				array(
+					'src' => $assetRoot . '80_2.png',
+					'width' => '89.5',
+					'height' => '37.99',
+					'top' => '29.5',
+					'left' => '8',
+				),
+				array(
+					'src' => $assetRoot . '80_3.png',
+					'width' => '73.96',
+					'height' => '14.68',
+					'top' => '79',
+					'left' => '13.02',
+				),
+			),
 		), array(
 			'background' => $assetRoot . '30y_bg.png',
 			'asset' => array(
@@ -152,31 +177,6 @@ $data = array(
 				),
 				array(
 					'src' => $assetRoot . '30y_3.png',
-					'width' => '73.96',
-					'height' => '14.68',
-					'top' => '79',
-					'left' => '13.02',
-				),
-			),
-		), array(
-			'background' => $assetRoot . '80_bg.png',
-			'asset' => array(
-				array(
-					'src' => $assetRoot . '80_1.png',
-					'width' => '41.12',
-					'height' => '1.33',
-					'top' => '4',
-					'left' => '29.44',
-				),
-				array(
-					'src' => $assetRoot . '80_2.png',
-					'width' => '89.5',
-					'height' => '37.99',
-					'top' => '29.5',
-					'left' => '8',
-				),
-				array(
-					'src' => $assetRoot . '80_3.png',
 					'width' => '73.96',
 					'height' => '14.68',
 					'top' => '79',
